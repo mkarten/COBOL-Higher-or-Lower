acdea4468b20db83b19b6e4700735090104d8eb6
mkarten <morgadoluca03@gmail.com>
2022-05-04 18:30:58 +0200

    main loop and IDENTIFICATION DIVISION filled

---
+++ Higher-or-Lower.cbl
@@ -0,0 +1,30 @@
+      ******************************************************************
+      * Author: Luca Morgado (mkarten)
+      * Date: 04-MAY-2022
+      * Purpose: Game
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+       PROGRAM-ID. HIGHER-OR-LOWER.
+       AUTHOR. LUCA MORGADO.
+       DATE-WRITTEN. 04-MAY-2022.
+       DATE-COMPILED. 04-MAY-2022.
+       SECURITY. OpenSource.
+       ENVIRONMENT DIVISION.
+      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+       CONFIGURATION SECTION.
+      *-----------------------
+       INPUT-OUTPUT SECTION.
+      *-----------------------
+       DATA DIVISION.
+      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+       FILE SECTION.
+      *-----------------------
+       WORKING-STORAGE SECTION.
+      *-----------------------
+       PROCEDURE DIVISION.
+      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+       MAIN-PROCEDURE.
+            STOP RUN.
+       END PROGRAM HIGHER-OR-LOWER.
