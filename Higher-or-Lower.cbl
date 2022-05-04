44091c73a892603e2461b6e41914794bb1b630d2
mkarten <morgadoluca03@gmail.com>
2022-05-04 19:53:25 +0200

    update readme and menu display and inputs

--- Higher-or-Lower.cbl
+++ Higher-or-Lower.cbl
@@ -22,9 +22,111 @@
        FILE SECTION.
       *-----------------------
        WORKING-STORAGE SECTION.
+       01  WS-USERNAME PIC X(7).
+       78  WS-NEW-LINE VALUE X"0D".
+       88  WS-RUN VALUE 1.
+       01  WS-HIGH-SCORE PIC ZZ9.
+       01  WS-ACTUAL-SCORE PIC ZZ9.
+       01  WS-MENU-INPUT PIC X(20).
+       01  WS-GAME-INPUT PIC X(20).
+       01  WS-GAME-DIFFICULTY PIC 9.
       *-----------------------
        PROCEDURE DIVISION.
       *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
        MAIN-PROCEDURE.
-            STOP RUN.
+           PERFORM GAME-INTRO.
+      *    PERFORM MAIN-GAME-LOOP UNTIL NOT WS-RUN.
+           STOP RUN.
+
+       GAME-INTRO.
+           SET WS-RUN TO TRUE
+           PERFORM DRAW-INTRO-SCREEN.
+           DISPLAY WS-NEW-LINE.
+           DISPLAY "Select your username (should be 7 letters or less)".
+           ACCEPT WS-USERNAME.
+           INSPECT WS-USERNAME REPLACING ALL WS-NEW-LINE BY SPACE.
+           DISPLAY "Well Hello " WS-USERNAME " and welcome to "
+           "HIGHER OR LOWER !".
+           DISPLAY WS-NEW-LINE.
+           PERFORM INTRO-MENU.
+
+       INTRO-MENU.
+           PERFORM DRAW-GAME-BAR.
+           DISPLAY "1 - Difficulty 1 (0 to 10) grants 1 point"
+           DISPLAY "2 - Difficulty 2 (0 to 100) grants 2 point"
+           DISPLAY "3 - Difficulty 3 (0 to 1000) grants 3 point"
+           DISPLAY "4 - Exit the game"
+           PERFORM DRAW-GAME-BAR.
+           PERFORM MENU-INPUT.
+
+       MENU-INPUT.
+           ACCEPT WS-MENU-INPUT
+           INSPECT WS-MENU-INPUT REPLACING ALL WS-NEW-LINE BY SPACE.
+           EVALUATE WS-MENU-INPUT
+               WHEN "1"
+                   DISPLAY WS-NEW-LINE "Setted difficulty to 1"
+                   MOVE 1 TO WS-GAME-DIFFICULTY
+               WHEN "2"
+                   DISPLAY WS-NEW-LINE "Setted difficulty to 2"
+                   MOVE 2 TO WS-GAME-DIFFICULTY
+               WHEN "3"
+                   DISPLAY WS-NEW-LINE "Setted difficulty to 3"
+                   MOVE 3 TO WS-GAME-DIFFICULTY
+               WHEN "4"
+                   DISPLAY WS-NEW-LINE "Exiting the game"
+                   PERFORM QUIT-GAME
+               WHEN OTHER
+                   DISPLAY "Invalid input please try again"
+                   PERFORM MENU-INPUT
+           END-EVALUATE.
+
+       DRAW-GAME-BAR.
+           DISPLAY
+           "-----------------------------------------------------------"
+           .
+
+       MAIN-GAME-LOOP.
+
+       GAME-INPUT.
+
+       RESET-INPUTS.
+           MOVE SPACES TO WS-MENU-INPUT.
+           MOVE SPACES TO WS-GAME-INPUT.
+
+       QUIT-GAME.
+           STOP RUN.
+
+       DRAW-INTRO-SCREEN.
+           DISPLAY
+           " _    _   _____    _____   _    _   ______   _____  ".
+           DISPLAY
+           "| |  | | |_   _|  / ____| | |  | | |  ____| |  __ \ ".
+           DISPLAY
+           "| |__| |   | |   | |  __  | |__| | | |__    | |__) |".
+           DISPLAY
+           "|  __  |   | |   | | |_ | |  __  | |  __|   |  _  / ".
+           DISPLAY
+           "| |  | |  _| |_  | |__| | | |  | | | |____  | | \ \ ".
+           DISPLAY
+           "|_|  |_| |_____|  \_____| |_|__|_| |______| |_|  \_\".
+           DISPLAY
+           "                 / __ \  |  __ \                    ".
+           DISPLAY
+           "                | |  | | | |__) |                   ".
+           DISPLAY
+           "                | |  | | |  _  /                    ".
+           DISPLAY
+           "                | |__| | | | \ \                    ".
+           DISPLAY
+           " _         ____  \____/  |_|  \_\ ______   _____    ".
+           DISPLAY
+           "| |       / __ \  \ \        / / |  ____| |  __ \   ".
+           DISPLAY
+           "| |      | |  | |  \ \  /\  / /  | |__    | |__) |  ".
+           DISPLAY
+           "| |      | |  | |   \ \/  \/ /   |  __|   |  _  /   ".
+           DISPLAY
+           "| |____  | |__| |    \  /\  /    | |____  | | \ \   ".
+           DISPLAY
+           "|______|  \____/      \/  \/     |______| |_|  \_\  ".
        END PROGRAM HIGHER-OR-LOWER.
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # COBOL-Higher-or-Lower
 A simple Higher or Lower game made using COBOL (Heck yeah thats old)
+
+Despite the fact that it is an old programing language it is still powering a lot of banking application
+For example it's used in the IBM Mainframe server or as we call it the big iron
