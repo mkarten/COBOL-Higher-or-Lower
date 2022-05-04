4bc2fb47e707000636789f2c7ab191229fb27ed5
mkarten <morgadoluca03@gmail.com>
2022-05-04 22:27:09 +0200

    game system and point sytem

--- Higher-or-Lower.cbl
+++ Higher-or-Lower.cbl
@@ -25,16 +25,22 @@
        01  WS-USERNAME PIC X(7).
        78  WS-NEW-LINE VALUE X"0D".
        01  WS-RUN PIC 9 VALUE 1.
-       01  WS-HIGH-SCORE PIC ZZ9.
-       01  WS-ACTUAL-SCORE PIC ZZ9.
+       01  WS-DISPLAY-SCORE PIC ZZZ9.
+       01  WS-ACTUAL-SCORE PIC 9999.
        01  WS-MENU-INPUT PIC X(20).
-       01  WS-GAME-INPUT PIC X(20).
+       01  WS-GAME-INPUT PIC X(5).
        01  WS-GAME-DIFFICULTY PIC 9.
-       01  WS-RANDOM-NB PIC ZZZ9.
+       01  WS-RAND PIC ZZZ9.
+       01  WS-NB-TEST-VALUE PIC X(5).
+       01  WS-IS-NB-FLAG PIC 9 VALUE 0.
+       01  WS-NB PIC ZZZ9.
+       01  WS-SEED PIC 9(08) VALUE 1.
       *-----------------------
        PROCEDURE DIVISION.
       *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
        MAIN-PROCEDURE.
+           MOVE 0 TO WS-ACTUAL-SCORE
+           COMPUTE WS-RAND = FUNCTION RANDOM(WS-SEED)
            PERFORM GAME-INTRO.
            PERFORM MAIN-GAME-LOOP UNTIL WS-RUN = 0.
            STOP RUN.
@@ -53,12 +59,15 @@
 
        INTRO-MENU.
            PERFORM DRAW-GAME-BAR.
+           MOVE WS-ACTUAL-SCORE TO WS-DISPLAY-SCORE
+           DISPLAY "Your Score :" WS-DISPLAY-SCORE
            DISPLAY "1 - Difficulty 1 (0 to 10) grants 1 point"
-           DISPLAY "2 - Difficulty 2 (0 to 100) grants 2 point"
-           DISPLAY "3 - Difficulty 3 (0 to 1000) grants 3 point"
+           DISPLAY "2 - Difficulty 2 (0 to 100) grants 5 point"
+           DISPLAY "3 - Difficulty 3 (0 to 1000) grants 10 point"
            DISPLAY "4 - Exit the game"
            PERFORM DRAW-GAME-BAR.
            PERFORM MENU-INPUT.
+           ACCEPT WS-SEED FROM TIME
            PERFORM GET-RANDOM-NUMBER.
 
        MENU-INPUT.
@@ -75,12 +84,9 @@
                    DISPLAY WS-NEW-LINE "Setted difficulty to 3"
                    MOVE 3 TO WS-GAME-DIFFICULTY
                WHEN "4"
-                   DISPLAY WS-NEW-LINE "Exiting the game"
                    PERFORM QUIT-GAME
                WHEN OTHER
                    DISPLAY "Invalid input please try again"
-                   PERFORM GET-RANDOM-NUMBER
-                   DISPLAY WS-RANDOM-NB
                    PERFORM MENU-INPUT
            END-EVALUATE.
            PERFORM RESET-INPUTS.
@@ -92,27 +98,64 @@
 
        MAIN-GAME-LOOP.
            PERFORM GAME-INPUT.
+           DISPLAY WS-RAND.
+           MOVE FUNCTION NUMVAL(WS-GAME-INPUT) TO WS-NB
+           EVALUATE WS-GAME-INPUT
+               WHEN "exit"
+                   PERFORM QUIT-GAME
+               WHEN OTHER
+                   IF WS-IS-NB-FLAG = 1 THEN
+                       IF WS-NB = WS-RAND THEN
+                           DISPLAY "you won" WS-NEW-LINE
+                           EVALUATE WS-GAME-DIFFICULTY
+                               WHEN 1
+                                   ADD 1 TO WS-ACTUAL-SCORE
+                               WHEN 2
+                                   ADD 5 TO WS-ACTUAL-SCORE
+                               WHEN 3
+                                   ADD 10 TO WS-ACTUAL-SCORE
+                           END-EVALUATE
+                           PERFORM INTRO-MENU
+                       ELSE IF WS-NB > WS-RAND THEN
+                           DISPLAY "LOWER"
+                       ELSE
+                           DISPLAY "HIGHER"
+                       END-IF
+                   ELSE
+                       DISPLAY "Invalid input please try again"
+                   END-IF
+           END-EVALUATE.
            PERFORM RESET-INPUTS.
 
        GAME-INPUT.
            DISPLAY "Guess the number !".
            ACCEPT WS-GAME-INPUT.
+           INSPECT WS-GAME-INPUT REPLACING ALL WS-NEW-LINE BY SPACES.
+           MOVE FUNCTION LOWER-CASE(WS-GAME-INPUT) TO WS-GAME-INPUT.
+           MOVE WS-GAME-INPUT TO WS-NB-TEST-VALUE.
+           INSPECT WS-NB-TEST-VALUE REPLACING ALL SPACES BY ZEROES.
+           IF WS-NB-TEST-VALUE IS NUMERIC THEN
+               MOVE 1 TO WS-IS-NB-FLAG
+           END-IF.
+
 
        RESET-INPUTS.
            MOVE SPACES TO WS-MENU-INPUT.
            MOVE SPACES TO WS-GAME-INPUT.
+           MOVE 0 TO WS-IS-NB-FLAG.
 
        QUIT-GAME.
+           DISPLAY WS-NEW-LINE "Exiting the game"
            STOP RUN.
 
        GET-RANDOM-NUMBER.
            EVALUATE WS-GAME-DIFFICULTY
                WHEN 1
-                   COMPUTE WS-RANDOM-NB = FUNCTION RANDOM () * 10 + 1
+                   COMPUTE WS-RAND = FUNCTION RANDOM(WS-SEED) * 10 + 1
                WHEN 2
-                   COMPUTE WS-RANDOM-NB = FUNCTION RANDOM () * 100 + 1
+                   COMPUTE WS-RAND = FUNCTION RANDOM(WS-SEED) * 100 + 1
                WHEN 3
-                   COMPUTE WS-RANDOM-NB = FUNCTION RANDOM () * 1000 + 1
+                   COMPUTE WS-RAND = FUNCTION RANDOM(WS-SEED) * 1000 + 1
            END-EVALUATE.
 
 
